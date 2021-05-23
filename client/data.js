24945a3f749f7737cdd8cdc770df7cc9525b888c
simplybychris <59068947+simplybychris@users.noreply.github.com>
2021-05-23 13:42:17 +0200

    Task done

--- client/data.js
+++ client/data.js
@@ -1,13 +1,10 @@
 let locations = new ReactiveVar([]);
 
-$.get('/data.json', function(data) {
-    if(typeof data === 'string')
-        data = JSON.parse(data);
-    
-    locations.set(data.locations);
-    console.log(locations.get());
+$.get("/data.json", function (data) {
+  if (typeof data === "string") data = JSON.parse(data);
+
+  locations.set(data.locations);
+  console.log(locations.get());
 });
 
-export {
-    locations
-}
+export { locations };
--- client/form.js
+++ client/form.js
@@ -1,51 +1,90 @@
-import "./form.html"
+import "./form.html";
 
-import { locations } from './data';
+import { locations } from "./data";
 import Pikaday from "pikaday";
+import { Session } from "meteor/session";
+import moment from "moment";
 
+let datepicker;
 Template.Form.onCreated(() => {
-
+  datepicker = new ReactiveVar(0);
 });
 
+Session.setDefault("hoursFrom", []);
+Session.setDefault("hoursTo", []);
+
 Template.Form.onRendered(() => {
-  this.$('select').select2({
+  this.$("select").select2({
     minimumResultsForSearch: 10,
-    placeholder: 'Select something...',
+    placeholder: "Select something...",
+  });
+
+  datepicker = new Pikaday({
+    field: $("#datepicker").get(0),
+    format: "YYYY-MM-DD",
+    minDate: new Date(),
+    onSelect: function () {
+      console.log(this.getMoment());
+    },
   });
 
   // max_advance_time = time in hours how far in the future to allow selection
   // max_duration = time in minutes how long one selection can take
-  new Pikaday({
-      field: $('#datepicker').get(0),
-      format: 'YYYY-MM-DD',
-      onSelect: function() {
-          console.log(this.getMoment());
-      }
-  })
 });
 
 Template.Form.helpers({
-  'locations': () => locations.get(),
+  locations: () => locations.get(),
 
-  'hoursFrom': () => {
-      return [
-          '08:00',
-          '08:30',
-          '09:00'
-      ];
+  hoursFrom: () => {
+    return Session.get("hoursFrom");
   },
 
-  'hoursTo': () => {
-      return [
-        '08:30',
-        '09:00',
-        '09:30',
-      ];
-  }
+  hoursTo: () => {
+    return Session.get("hoursTo");
+  },
 });
 
 Template.Form.events({
-    'change #location': (e) => {
-        console.log(e.target.value);
+  "change #location": (e) => {
+    const locationsData = locations.get();
+
+    let selectedLocation = "";
+    for (let i = 0; i < locationsData.length; i++) {
+      if (e.target.value == locationsData[i].id) {
+        console.log("Picked location: ", locationsData[i]);
+        selectedLocation = locationsData[i];
+        /*
+        {{#each}} currently only accepts arrays, cursors or falsey values
+
+        if time_from inside json object is an array => add to session, 
+        else => put it in the array and add to session 
+        */
+        if (Array.isArray(locationsData[i].time_from)) {
+          Session.set("hoursFrom", locationsData[i].time_from);
+        } else {
+          Session.set("hoursFrom", [locationsData[i].time_from]);
+        }
+
+        if (Array.isArray(locationsData[i].time_to)) {
+          Session.set("hoursTo", locationsData[i].time_to);
+        } else {
+          Session.set("hoursTo", [locationsData[i].time_to]);
+        }
+      }
     }
+
+    const hours = selectedLocation.max_advance_time;
+    /*
+    if there is provided max_advance_time => set max date range,
+    else => reset max range
+    */
+    if (hours !== null) {
+      const maxDate = moment(moment()).add(hours, "hours").toDate();
+      datepicker.setMaxDate(maxDate);
+    } else {
+      datepicker.setMaxDate();
+    }
+    //reset input value
+    $("#datepicker").val("Select another date");
+  },
 });
